a83d9c900df41518c0cf4bac1212ab4121c16160
Godin <godin.fake@gmail.com>
2025-07-10 15:20:06 -0300

    Update convert.py
    Em progresso - 10072025

--- Challenges/convert.py
+++ Challenges/convert.py
@@ -4,5 +4,23 @@ C = 12
 D = 13
 E = 14
 F = 15
-hex = [0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F]
+hexadecimal = [0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F]
 hex_str = 'hexNum'
+
+def convert_to_decimal(hex_num):
+    decimal_value = 0
+    for i, digit in enumerate(reversed(hex_num)):
+        decimal_value += hexadecimal.index(digit) * (16 ** i)
+    return decimal_value
+if hex_str == 'hexNum':
+    print('It is a hexadecimal number')
+else:
+    print(None)
+    
+Resposta = hex_str(input('Aguardando valor: '))
+
+convert_to_decimal(Resposta)
+
+print(convert_to_decimal(Resposta))
+    
+    
